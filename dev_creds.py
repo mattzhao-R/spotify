d96b388628ae807f454dc1ecf8f8606308f92f6e
Matthew Zhao <matthewhrzhao@gmail.com>
2022-05-18 16:01:42 -0500

    dev_creds template

--- dev_creds.py
+++ dev_creds.py
@@ -2,6 +2,6 @@ import spotipy as sp
 from spotipy.oauth2 import SpotifyClientCredentials
 
 def get_creds():
-    cid = 'c210de964165497b851bdb12688c548b'
-    c_secr = 'd7bbca6aaa334d71bed52eac97516495'
+    cid = 'client_id' # your client id here
+    c_secr = 'client_secret' # your client secret here
     return cid,c_secr
