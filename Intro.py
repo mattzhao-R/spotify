4922fd3bcf4bb83863b75bcaaa0d0950efb53645
Matthew Zhao <matthewhrzhao@gmail.com>
2022-09-10 13:51:47 -0400

    write intro page

--- Intro.py
+++ Intro.py
@@ -13,10 +13,14 @@ st.sidebar.success("Sections")
 
 st.markdown(
 """
-## Overview 
+### Introduction
+
+Hello! My name is Matthew Zhao and I am a junior at the University of Chicago studying Economics and Data Science. This website showcases my project on Spotify song classification and listening patterns. 
+
+The inspiration for the various analyses in this project comes from my own interest in making cohesive Spotify playlists for my own and my friends' enjoyment, and looks to more closely examine the relationship between songs' assigned genres and its actual audio features. It then attempts to 're-classify' these songs into new genre groupings based on these features and finally visualizes the listening patterns of these new genre clusters using an interactive calendar-like format. 
 
 
-## Sections
+### Sections
 * Summary
 * Data Generation Process & Exploration
 * Detecting Genres from Features
