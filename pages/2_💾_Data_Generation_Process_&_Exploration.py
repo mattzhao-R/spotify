8e4d664f92f847333b36db6616671af2ad5d6329
Matthew Zhao <matthewhrzhao@gmail.com>
2022-09-12 19:01:30 -0400

    fixed spacing

--- pages/2_💾_Data_Generation_Process_&_Exploration.py
+++ pages/2_💾_Data_Generation_Process_&_Exploration.py
@@ -14,12 +14,15 @@ st.markdown(
 In this section I will describe how I generated my dataset. 
 
 *Spotify Streaming History*
+
 First, I requested my past year's Spotify streaming history from Spotify. You can find the link describing how to do so [here](https://support.spotify.com/us/article/data-rights-and-privacy-settings/?ref=related). This data included the time when I started playing the song down to the minute in UTC, the song and artist names, and the duration I listened to the song for in milliseconds. 
 
 *Getting Audio Features from Spotify API*
+
 From here, I wrote a series of functions which would allow me to get the audio features for each song using parallel processing via the Spotify API. This process mimics how parallelization works for Selenium workers in webscraping, with each Spotify 'worker' being fed into the queue for parallelization. 
 
 *Webscraping Genres*
+
 I then needed to get the genre for each song since there was an issue with the Spotify API where 90% of artists did not have genres assigned to them. As a result, I turned to webscraping to get the genre for each song from a website called [last.fm](https://www.last.fm/). Scraping the genre for each song resulted in high variance and inconsistent genre assignments so I instead scraped the genre for each artist and assigned those to each song. This was done using BeautifulSoup and multiprocessing pool. 
 
 The resulting dataframe from the DGP can be seen below.
